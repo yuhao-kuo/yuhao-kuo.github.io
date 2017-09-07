638391d230aed0386f3f1e2283e3c60adf8cbcfd
Yu-Hao Kuo <yuhao-kuo@users.noreply.github.com>
2017-09-08 00:14:10 +0800

    test main.js
    test main.js

--- js/main.js
+++ js/main.js
@@ -19,11 +19,11 @@ $(document).ready(function () {
 
   /*if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
-  }*/
+  }
 
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
-  }
+  }*/
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
